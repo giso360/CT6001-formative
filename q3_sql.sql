dbc453b8a68277b2a9e2afb78e1858602a877686
giso360 <neskouhe00@gmail.com>
2020-11-15 14:17:05 +0200

    Update q3_sql.sql

--- q3_sql.sql
+++ q3_sql.sql
@@ -10,17 +10,33 @@ SELECT distinct name FROM supplierproduct.product;
 
 select distinct name from product right join supplierproduct2 on product.id = supplierproduct2.pid where qty > 400;
 
+SELECT distinct product.name
+from product, supplierproduct2
+where product.id=supplierproduct2.pid
+and supplierproduct2.qty>400;
+
 -- 4.	Write a query to count the number of suppliers
 
 select count(distinct name) from supplier;
+select count(*) from supplier;
 
 -- 5.	Display the total number of products
 
 select count(distinct name) from product;
+select count(*) from product;
 
 -- 6.	Count the total number of suppliers that are offering product P1
 
 select count(sid) from supplierproduct2 where pid='P1';
+
+select count(supplier.name)
+from supplier, supplierproduct2
+where supplier.id=supplierproduct2.sid
+and supplierproduct2.pid='P1';
+
+select count(distinct supplierproduct2.sid) as N_suppliers
+from supplierproduct2
+where supplierproduct2.pid='P1';
 
 -- 7.	Write a query to display the names of products that smith is offering.
 
